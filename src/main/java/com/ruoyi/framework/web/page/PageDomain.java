9ee8f8db57e43777969d97cbc040432824714c51
lnsylt <lnsylt@localhost.localdomain>
2020-05-25 12:05:03 +0900

    更新

--- src/main/java/com/ruoyi/framework/web/page/PageDomain.java
+++ src/main/java/com/ruoyi/framework/web/page/PageDomain.java
@@ -24,7 +24,31 @@ public class PageDomain
         {
             return "";
         }
-        return StringUtils.toUnderScoreCase(orderByColumn) + " " + isAsc;
+        
+        String[] orderByCols= orderByColumn.split(",");
+        String[] isAscs= isAsc.split(",");
+        
+        String resultOrderBy ="";
+        
+        if(orderByCols.length == isAscs.length) {
+        	if(orderByCols.length != 1) {        		
+        		for(int i=0;i<orderByCols.length;i++) {
+        			resultOrderBy = resultOrderBy+StringUtils.toUnderScoreCase(orderByCols[i]) + " " + isAscs[i]+",";
+        		}
+        		resultOrderBy = resultOrderBy.substring(0, resultOrderBy.length()-1);
+        	}else {
+        		resultOrderBy=StringUtils.toUnderScoreCase(orderByColumn) + " " + isAsc;
+        	}
+        }else {
+        	if(orderByCols.length > isAscs.length) {
+        		for(int i=0;i<orderByCols.length;i++) {
+        			resultOrderBy = resultOrderBy+StringUtils.toUnderScoreCase(orderByCols[i]) + " " + isAscs[0]+",";
+        		}
+        		resultOrderBy = resultOrderBy.substring(0, resultOrderBy.length()-1);
+        	}
+        }
+        
+        return resultOrderBy;
     }
 
     public Integer getPageNum()
